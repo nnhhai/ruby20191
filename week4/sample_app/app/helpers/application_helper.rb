8000c274d30bc7ea40b4e2631926de80ed6787f7
Nhat Hai <nhathai.bigfoot@gmail.com>
2019-12-30 20:17:55 +0700

    update sample_app

--- week4/sample_app/app/helpers/application_helper.rb
+++ week4/sample_app/app/helpers/application_helper.rb
@@ -1,11 +1,6 @@
-module ApplicationHelper 
-    # Return the full title on a per-page basis
-    def full_title(page_title = '')
-        base_title = "Ruby on Rails tutorial Sammple App"
-        if page_title.empty?
-            base_title
-        else
-            "#{page_title}|#{base_title}"
-        end
-    end
+module ApplicationHelper
+  def full_title page_title = ""
+    base_title = "Ruby on Rails Tutorial Sample App"
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
 end
--- week4/sample_app/config/application.rb
+++ week4/sample_app/config/application.rb
@@ -1,19 +1,34 @@
+require File.expand_path('../boot', __FILE__)
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    I18n.load_path += Dir[Rails.root.join("lib", "locale", "*.{rb,yml}")]
+
+    I18n.available_locales = [:en, :vi]
+
+    I18n.default_locale = :en
+
+    I18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
+
+    config.generators.system_tests = nil
+
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
